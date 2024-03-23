6f2b54fc04fcd38e96abbc891fe159c9f1324468
Pepe57 <104223887+Pepe-57@users.noreply.github.com>
2024-03-23 14:18:47 +0200

    Created programs.h

---
+++ programs/release-v1.0-programs/programs.h
@@ -0,0 +1,12 @@
+#ifndef PROGRAMS_H
+#define PROGRAMS_H
+
+class programs {
+public:
+    void my_program(); // Your program name
+    void calculator();
+    void direct_syscall();
+};
+
+extern programs program;
+#endif
