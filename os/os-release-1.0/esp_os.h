fe1edc70dfe4512672c5498a8a30b104479abed7
Pepe57 <104223887+Pepe-57@users.noreply.github.com>
2024-03-23 14:22:10 +0200

    Create esp_os.h

---
+++ os/os-release-1.0/esp_os.h
@@ -0,0 +1,12 @@
+#ifndef ESP_OS_H
+#define ESP_OS_H
+
+#define osversion "OS: Esp-os Release 1.0"
+
+class operating_system {
+public:
+    int terminal();
+};
+
+extern operating_system OperatingSystem;
+#endif
