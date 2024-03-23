cb268526784d6ea81eaa986f11817cdd3f9c2a94
Pepe57 <104223887+Pepe-57@users.noreply.github.com>
2024-03-24 01:45:12 +0200

    Created io_driver.h

---
+++ drivers/v1.0.1/io_driver.h
@@ -0,0 +1,19 @@
+#ifndef IO_DRIVER_H
+#define IO_DRIVER_H
+
+class io_driver {
+public:
+    int get_pin(int pin);
+    void set_pin(int pin, int value);
+    void reset_pins();
+};
+
+
+extern int io_pin1;
+extern int io_pin2;
+extern int io_pin3;
+extern int io_pin4;
+extern int led;
+
+extern io_driver io_dv;
+#endif
